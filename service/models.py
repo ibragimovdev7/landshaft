a4351092cbbbdd5a8c9d675ef93efd0460be638b
ibragimovdev7 <89067487+ibragimovdev7@users.noreply.github.com>
2022-02-27 04:45:14 +0500

    second

--- service/models.py
+++ service/models.py
@@ -4,6 +4,7 @@ class Service(models.Model):
     name = models.CharField(max_length=100)
     image = models.ImageField(null=True,blank=True)
     show = models.BooleanField(default=False)
+    slug = models.TextField(max_length=20,unique=True)
     description = models.TextField()
 
     def __str__(self):
